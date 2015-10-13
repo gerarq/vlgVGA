6cfa1fbe175c204a4fe791e812abc039f8268c63
gerarq <g.israel.molina@gmail.com>
2015-10-13 14:49:02 -0500

    feature
    fractal pattern implementation

--- pixel_gen.v
+++ pixel_gen.v
@@ -4,6 +4,6 @@ input wire [9:0]x_pxl,y_pxl,
 output wire [11:0]rgb
 );
 
-assign rgb = (&x_pxl ) ? {12{1'b0}} : {4'b1111,{4{1'b0}},4'b0000};
+assign rgb = (x_pxl&y_pxl ) ? {12{1'b0}} : {4'b1111,{4{1'b0}},4'b0000};
 
 endmodule
